a155c8670348ffed70e036020333e14dee976016
wlad999 <wladmail25@gmeil.com>
2020-05-25 10:15:28 +0300

    with utils

--- src/App.js
+++ src/App.js
@@ -1,52 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-import { v4 as uuidv4 } from "uuid";
-import { setItems, getItems, setDefaultItem, onDragEnd } from "./utils/utils";
-
-//const itemsFromBackend = [
-//  { id: uuidv4(), content: "First" },
-//  { id: uuidv4(), content: "Second" },
-//  { id: uuidv4(), content: "Third" },
-//  { id: uuidv4(), content: "Fourth" },
-//  { id: uuidv4(), content: "Fifth" },
-//  { id: uuidv4(), content: "Sixth" },
-//  { id: uuidv4(), content: "Seventh" },
-//];
-
-//const columnsFromBackend = {
-//  [uuidv4()]: { name: "Cash Offer", items: [] },
-//  [uuidv4()]: { name: "Finance", items: [] },
-//  [uuidv4()]: { name: "Lease", items: [] },
-//  [uuidv4()]: { name: "Other", items: itemsFromBackend },
-//};
-//const savedItem = getItems();
-//let defaultItems = savedItem ? savedItem : columnsFromBackend;
-//const onDragEnd = async (result, columns, setColumns) => {
-//  if (!result.destination) return;
-//  const { source, destination } = result;
-//  if (source.droppableId !== destination.droppableId) {
-//    const sourceColumn = columns[source.droppableId];
-//    const destColumn = columns[destination.droppableId];
-//    const sourceItems = [...sourceColumn.items];
-//    const destItems = [...destColumn.items];
-//    const [removed] = sourceItems.splice(source.index, 1);
-//    destItems.splice(destination.index, 0, removed);
-//    setColumns({
-//      ...columns,
-//      [source.droppableId]: { ...sourceColumn, items: sourceItems },
-//      [destination.droppableId]: { ...destColumn, items: destItems },
-//    });
-//  } else {
-//    const column = columns[source.droppableId];
-//    const copiedItems = [...column.items];
-//    const [removed] = copiedItems.splice(source.index, 1);
-//    copiedItems.splice(destination.index, 0, removed);
-//    setColumns({
-//      ...columns,
-//      [source.droppableId]: { ...column, items: copiedItems },
-//    });
-//  }
-//};
+import { setItems, setDefaultItem, onDragEnd } from "./utils/utils";
 
 function App() {
   let defaultItems = setDefaultItem();
