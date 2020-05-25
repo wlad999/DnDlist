32d74102267cdb41d40264bf9e763e92fdc679b7
wlad999 <wladmail25@gmeil.com>
2020-05-25 10:36:26 +0300

    Updates

---
+++ precache-manifest.e5e3df8fa699ecf8336ac0365cc5b6b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6df5e4cf1ec0c8b14ad6f33ea30a1f3d",
+    "url": "/DnDlist/index.html"
+  },
+  {
+    "revision": "c2757c4f6e4103a98613",
+    "url": "/DnDlist/static/js/2.1204f6cc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/DnDlist/static/js/2.1204f6cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6886205a09377f3d1f9e",
+    "url": "/DnDlist/static/js/main.16043f44.chunk.js"
+  },
+  {
+    "revision": "a118f6724d3b16da5e6a",
+    "url": "/DnDlist/static/js/runtime-main.ecdb43e6.js"
+  }
+]);
